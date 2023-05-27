33acbc6ea7a3fec21f7eb3d53b492aff1efc83f5
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-28 03:07:22 +0530

    Added loaders

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react"
 
+import Loading from './Loading'
+
 const Card = () => {
     const [quote, setQuote] = useState(null)
     const [author, setAuthor] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     const fetchQuote = async () => {
         setLoading(true)
@@ -19,24 +22,48 @@ const Card = () => {
 
   return (
     <article className="flex flex-col justify-center sm:justify-around items-center gap-y-4 sm:gap-y-5 w-10/12 max-w-xl p-6 sm:p-8 bg-white rounded-lg sm:rounded-2xl drop-shadow-2xl">
-        
+
+        {/* title */}
         <div className="w-full text-center font-semibold text-xl sm:text-3xl">
-            Quotes of the day
+            {
+                loading ?
+                <Loading className="w-1/2 h-6 m-auto" /> :
+                'Quotes of the day'
+            }
         </div>
 
+        {/* quote */}
         <div className="w-full sm:w-11/12">
-            <p className="text-gray-600 text-center font-light text-sm sm:text-lg">
-                {quote}
-            </p>
-            <span className="text-sky-900 block w-full text-right text-xs sm:text-base italic">
-                ⎯⎯ &nbsp;
-                {author}
-            </span>
+            {/* content */}
+            {
+                loading ?
+                <div className="flex flex-col gap-y-2">
+                    <Loading className="w-full h-3 mt-5 mx-auto" />
+                    <Loading className="w-full h-3 mx-auto" />
+                    <Loading className="w-3/4 h-3" /> 
+                </div> :
+                <p className="mb-3 text-gray-600 text-center font-light text-sm sm:text-lg">
+                    {quote}
+                </p>
+            }
+
+            {/* author */}
+            {
+                loading ?
+                <Loading className="w-2/5 h-3 mt-5 ml-auto" /> :
+                <span className="text-sky-900 block w-full text-right text-xs sm:text-base italic">
+                    ⎯⎯ &nbsp;
+                    {author}
+                </span>
+            }
         </div>
 
-        <div className="w-full">
-            
-        </div>
+        {/* actions */}
+        {
+            !loading &&
+            <div className="w-full">
+            </div>
+        }
 
     </article>
   )
---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = ({ className }) => {
+  return (
+    <div className={`animate-pulse bg-slate-300 rounded ${className}`}></div>
+  )
+}
+
+export default Loading
